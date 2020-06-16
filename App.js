82233542c4b187b383f1c45766609b59fe72ffdf
Thomas <thomas.otxoa@gmail.com>
2020-06-16 11:44:41 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Button, Platform, StyleSheet, Text, View, Alert } from 'react-native';
+
+const instructions = Platform.select({
+  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
+  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
+  web: 'coucou'
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to EduGame!</Text>
+      <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <Text style={styles.instructions}>{instructions}</Text>
+      <Button
+          title="Press me"
+          onPress={() => Alert.alert('Simple Button pressed')}
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
